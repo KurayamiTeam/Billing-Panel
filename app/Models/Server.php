dfefad5df88d35f41a1b9b93bc0c7410f23ace92
Félix Ofx <xlfelixgamer@gmail.com>
2026-06-01 14:43:06 -0400

    Crear Server.php

---
+++ app/Models/Server.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Server extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'pterodactyl_id',
+        'uuid',
+        'name',
+        'status',
+        'cpu',
+        'ram',
+        'disk',
+        'expires_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
