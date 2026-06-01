de3464e89a60b705bfd9d1922a1d3ebf2c5d1262
Félix Ofx <xlfelixgamer@gmail.com>
2026-06-01 14:42:32 -0400

    Crear create_servers_table.php

---
+++ database/migrations/create_servers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('servers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('pterodactyl_id')->unique();
+            $table->string('uuid')->unique();
+            $table->string('name');
+            $table->string('status')->default('active');
+            $table->integer('cpu');
+            $table->integer('ram');
+            $table->integer('disk');
+            $table->date('expires_at');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('servers');
+    }
+};
