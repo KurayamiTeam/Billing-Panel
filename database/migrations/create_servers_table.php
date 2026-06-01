cc1cf2648cefae9e506665f823c86150b0db9537
Félix Ofx <xlfelixgamer@gmail.com>
2026-06-01 14:53:39 -0400

    Actualizar create_servers_table.php

--- database/migrations/create_servers_table.php
+++ database/migrations/create_servers_table.php
@@ -11,7 +11,8 @@ return new class extends Migration
         Schema::create('servers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->integer('pterodactyl_id')->unique();
+            $table->string('driver')->default('pterodactyl');
+            $table->integer('external_id')->unique();
             $table->string('uuid')->unique();
             $table->string('name');
             $table->string('status')->default('active');
